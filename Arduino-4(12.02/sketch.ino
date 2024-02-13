85a5bb4c834411e405f837ba10679c1f1c4120c3
Klim <114877716+TiKlim@users.noreply.github.com>
2024-02-13 15:35:18 +0300

    Add files via upload

--- Arduino-4(12.02/sketch.ino
+++ Arduino-4(12.02/sketch.ino
@@ -1,10 +1,13 @@
 void setup() {
 pinMode(4, OUTPUT);
+digitalWrite(4, HIGH); //Включить LED
+delay(3000);
+digitalWrite(4, LOW);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(4, HIGH); //Включить LED
-delay(1000);
+digitalWrite(4, HIGH);
+delay(500);
 digitalWrite(4, LOW);
 delay(1000); 
 }
